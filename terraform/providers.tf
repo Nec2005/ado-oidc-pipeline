1b8346a7cca98c49ac5572c9309b990f98bee106
necatiu <necatiu@chillipharm.com>
2025-12-19 12:06:46 +0000

    updated AZCli task

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -29,7 +29,7 @@ provider "aws" {
 
 provider "azuredevops" {
   org_service_url = var.azuredevops_org_service_url
-  # client_id            = "eea374a1-4536-49c5-b857-609555d7060b"
-  # tenant_id            = "7fb00207-c2a9-42ba-b119-d3ed58581456"
+  client_id            = "1a550dca-bd4e-4c4d-8916-845a818246a3"
+  tenant_id            = "7fb00207-c2a9-42ba-b119-d3ed58581456"
   use_oidc = true
 }
