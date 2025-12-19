2ee9c4231696e0cd6cad2e685d5d03cb05816aea
necatiu <necatiu@chillipharm.com>
2025-12-19 14:22:01 +0000

    updated provider version

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -28,9 +28,6 @@ terraform {
 
 provider "aws" {
   region = var.aws_region
-  # assume_role {
-  #   role_arn = "arn:aws:iam::533267146698:role/ADODeploymentRole"
-  # }
 }
 
 provider "azuredevops" {
